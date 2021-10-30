e8aa5a27831f90f1e1c852f1242c03e5a4150511
dxkite <dxkite@163.com>
2021-10-30 21:53:15 +0800

    feat: 支持命令行读入数据

--- cmd/usercenter/main.go
+++ cmd/usercenter/main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
+	"dxkite.cn/log"
 	"dxkite.cn/usercenter/server"
 	"dxkite.cn/usercenter/store/leveldb"
 	"flag"
-	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
 	data := flag.String("data", "./data", "data save path")
-	addr := flag.String("addr", ":2333", "listen addr")
+	addr := flag.String("addr", ":8888", "listen addr")
 	flag.Parse()
 
 	us, err := leveldb.NewUserStore(*data)
 	if err != nil {
-		fmt.Println("open database error", err)
+		log.Error("open database error", err)
 		os.Exit(1)
 	}
 
 	if err := us.Init(0); err != nil {
-		fmt.Println("database init error", err)
+		log.Error("database init error", err)
 		os.Exit(1)
 	}
 
+	log.Info("start server", *addr)
 	_ = http.ListenAndServe(*addr, server.NewUserServer(us))
 }
